4f160b2470220b777f010ca77165ca21f09b2363
Daniel McCloy <dan.mccloy@gmail.com>
2014-09-22 13:05:12 +0800

    aggregation collapsed to a single master script

--- code/aggregation/merge-genealogy.R
+++
@@ -1,8 +0,0 @@
-# Place holder for merging genealogy sources.
-
-# Useful links:
-
-https://github.com/clld/wals-data
-http://wals.info/download
-http://wals.info/static/download/wals-language.csv.zip
-
