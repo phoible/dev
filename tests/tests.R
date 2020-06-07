1a0e20e73083d92b3f01f5600f341cda98a8b9df
Daniel McCloy <dan@mccloy.info>
2020-06-07 16:39:19 -0700

    update metadata test

--- tests/tests.R
+++ tests/tests.R
@@ -1,11 +1,20 @@
-# phoible data tests
+#! /usr/bin/env Rscript
+
+## This script checks that each metadata field associated with each inventory
+## has only one value within each inventory.
 
 library(dplyr)
-library(testthat)
 
-load('../data/phoible.RData')
+## load PHOIBLE data
+phoible_data_file  <- file.path("..", "data", "phoible.csv")
+phoible_col_types <- readr::cols(InventoryID="i", Marginal="l", .default="c")
+phoible <- readr::read_csv(phoible_data_file, col_types=phoible_col_types)
 
-# Metadata categories should be unique within inventories
-ids <- phoible %>% select(InventoryID) %>% distinct()
-metadata <- phoible %>% select(InventoryID, Glottocode, ISO6393, LanguageName, SpecificDialect, Source) %>% distinct()
-expect_equal(nrow(ids), nrow(metadata))
+## should be one distinct row
+phoible %>%
+    group_by(InventoryID) %>%
+    select(Glottocode, ISO6393, LanguageName, SpecificDialect, Source) %>%
+    n_distinct() ->
+    n_distinct_metadata
+
+testthat::expect_equal(n_distinct_metadata, n_distinct(phoible$InventoryID))
