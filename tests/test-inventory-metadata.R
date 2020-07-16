968acae35bff02d7b9b43c4d3c2ff8101bdd09fb
Daniel McCloy <dan@mccloy.info>
2020-07-16 14:28:13 -0700

    custom expectations -> much cleaner failure messages

--- tests/test-inventory-metadata.R
+++ tests/test-inventory-metadata.R
@@ -21,7 +21,7 @@ phoible_col_types <- readr::cols(InventoryID="i", Marginal="l", .default="c")
 phoible <- readr::read_csv(phoible_data_file, col_types=phoible_col_types)
 
 
-test_that("all ISO codes are valid", {
+test_that("language codes are valid", {
     ## set global options (restored automatically on exit)
     withr::local_options(list(stringsAsFactors=FALSE))
 
@@ -32,59 +32,55 @@ test_that("all ISO codes are valid", {
     iso_table <- readr::read_tsv(iso_url, col_types=default_char_cols)
     iso_valid <- pull(iso_table, Id)
 
-    ## pull out the relevant columns and compare to reference list
-    iso_phoible <- pull(phoible, ISO6393)
-    iso_invalid <- setdiff(iso_phoible, iso_valid)
-    if(length(iso_invalid)) {
-        withr::deferred_run({
-            cat("\n\n==================")
-            cat("\nINVALID ISO CODES:\n")
-            cat(iso_invalid)
-            cat("\n==================\n")
-        })
-    }
-    expect_length(iso_invalid, 0)
-})
-
-
-test_that("all glottocodes are valid", {
-    ## set global options (restored automatically on exit)
-    withr::local_options(list(stringsAsFactors=FALSE))
-
-    default_char_cols <- readr::cols(.default="c")
-
     ## load glottolog
     glotto_url <- "https://github.com/glottolog/glottolog-cldf/blob/master/cldf/languages.csv?raw=true"
     glotto_table <- readr::read_csv(glotto_url, col_types=default_char_cols)
     glotto_valid <- pull(glotto_table, ID)
 
     ## pull out the relevant columns and compare to reference list
+    iso_phoible <- pull(phoible, ISO6393)
+    iso_invalid <- setdiff(iso_phoible, iso_valid)
     glotto_phoible <- pull(phoible, Glottocode)
     glotto_invalid <- setdiff(glotto_phoible, glotto_valid)
-    if(length(glotto_invalid)) {
-        withr::deferred_run({
-            cat("\n\n====================")
-            cat("\nINVALID GLOTTOCODES:\n")
-            cat(glotto_invalid)
-            cat("\n====================\n")
-        })
+
+    ## test
+    expect(length(iso_invalid) == 0,
+           paste("INVALID ISO CODES:", paste(iso_invalid, collapse=" "),
+                 sep="\n")
+           )
+
+    expect(length(glotto_invalid) == 0,
+           paste("INVALID GLOTTOCODES:", paste(glotto_invalid, collapse=" "),
+                 sep="\n")
+           )
     }
-    expect_length(glotto_invalid, 0)
-})
+)
 
 
 test_that("metadata is unique within each inventory", {
-    ## extract metadata columns and count unique rows
-    phoible %>%
-        group_by(InventoryID) %>%
-        select(Glottocode, ISO6393, LanguageName, SpecificDialect, Source) %>%
-        n_distinct() ->
-        n_distinct_metadata_rows
     ## count inventories
     phoible %>%
         select(InventoryID) %>%
         n_distinct() ->
         n_inventories
+
+    ## extract metadata columns. We need the intermediate dataframe for the
+    ## the error message, which is why we don't just count with n_distinct().
+    phoible %>%
+        group_by(InventoryID) %>%
+        select(Glottocode, ISO6393, LanguageName, SpecificDialect, Source) %>%
+        unique() ->
+        distinct_metadata_rows
+
+    distinct_metadata_rows %>%
+        filter(n() > 1) %>%
+        pull(InventoryID) %>%
+        paste(collapse=" ") ->
+        errors
+
     ## test
-    expect_equal(n_distinct_metadata_rows, n_inventories)
-    })
+    expect(nrow(distinct_metadata_rows) == n_inventories,
+           paste("INVENTORIES WITH NON-UNIQUE METADATA:", errors, sep="\n")
+           )
+    }
+)
--- tests/test-phonemes.R
+++ tests/test-phonemes.R
@@ -14,26 +14,16 @@ phoible_col_types <- readr::cols(InventoryID="i", Marginal="l", .default="c")
 phoible <- readr::read_csv(phoible_data_file, col_types=phoible_col_types)
 
 test_that("inventories don't have duplicate phonemes", {
-    ## set global options (restored automatically on exit)
-    withr::local_options(list(stringsAsFactors=FALSE))
-
-    ## count 'em up
-    phoible %>%
-        group_by(InventoryID) %>%
-        summarise(all_unique=n() == n_distinct(Phoneme), .groups="drop") ->
-        results_table
     ## show the failures
-    if(any(!results_table$all_unique)) {
-        withr::deferred_run({
-            cat("\n\n====================================")
-            cat("\nINVENTORIES WITH DUPLICATE PHONEMES:\n")
-            results_table %>%
-                filter(!all_unique) %>%
-                pull(InventoryID) %>%
-                print()
-            cat("\n====================================\n")
-        })
-    }
+    phoible %>%
+        group_by(InventoryID, Phoneme) %>%
+        select(InventoryID, Phoneme) %>%
+        filter(n() > 1) ->
+        duplicates
 
-    expect_true(all(pull(results_table, all_unique)))
-    })
+    expect(nrow(duplicates) == 0,
+    	   paste(c("INVENTORIES WITH DUPLICATE PHONEMES:",
+    	   	       capture.output(print(duplicates))), sep="\n")
+    	   )
+    }
+)
