128eb6ee30c1c5d762f165a12c7695456ed41088
Daniel McCloy <dan@mccloy.info>
2020-07-16 11:37:03 -0700

    merge related tests into one file

--- tests/test-language-code-validity.R
+++ tests/test-language-code-validity.R
@@ -10,38 +10,56 @@
 ## Full list of retired codes:
 ## http://www-01.sil.org/iso639-3/codes_retired.asp
 
-testthat::context("Language code validity")
+library(dplyr)
 
-## set global options (restored at end)
-saf <- getOption("stringsAsFactors")
-options(stringsAsFactors=FALSE)
-
-def_char_cols <- readr::cols(.default="c")
-
-## load official ISO table
-iso_url <- "http://www-01.sil.org/iso639-3/iso-639-3.tab"
-iso_table <- readr::read_tsv(iso_url, col_types=def_char_cols)
-iso_valid <- dplyr::pull(iso_table, Id)
-
-## load glottolog
-glotto_url <- "https://github.com/glottolog/glottolog-cldf/blob/master/cldf/languages.csv?raw=true"
-glotto_table <- readr::read_csv(glotto_url, col_types=def_char_cols)
-glotto_valid <- dplyr::pull(glotto_table, ID)
+testthat::context("Inventory metadata")
 
 ## load PHOIBLE data
 phoible_data_file  <- file.path("..", "data", "phoible.csv")
 phoible_col_types <- readr::cols(InventoryID="i", Marginal="l", .default="c")
 phoible <- readr::read_csv(phoible_data_file, col_types=phoible_col_types)
 
-## pull out the relevant columns and compare to reference lists
-iso_phoible <- dplyr::pull(phoible, ISO6393)
-iso_invalid <- dplyr::setdiff(iso_phoible, iso_valid)
 
-glotto_phoible <- dplyr::pull(phoible, Glottocode)
-glotto_invalid <- dplyr::setdiff(glotto_phoible, glotto_valid)
+testthat::test_that("all language codes are valid", {
+    ## set global options (restored automatically on exit)
+    withr::local_options(list(stringsAsFactors=FALSE))
 
-testthat::expect_length(iso_invalid, 0)
-testthat::expect_length(glotto_invalid, 0)
+    default_char_cols <- readr::cols(.default="c")
 
-## reset options
-options(stringsAsFactors=saf)
+    ## load official ISO table
+    iso_url <- "http://www-01.sil.org/iso639-3/iso-639-3.tab"
+    iso_table <- readr::read_tsv(iso_url, col_types=default_char_cols)
+    iso_valid <- pull(iso_table, Id)
+
+    ## load glottolog
+    glotto_url <- "https://github.com/glottolog/glottolog-cldf/blob/master/cldf/languages.csv?raw=true"
+    glotto_table <- readr::read_csv(glotto_url, col_types=default_char_cols)
+    glotto_valid <- pull(glotto_table, ID)
+
+    ## pull out the relevant columns and compare to reference lists
+    iso_phoible <- pull(phoible, ISO6393)
+    iso_invalid <- setdiff(iso_phoible, iso_valid)
+
+    glotto_phoible <- pull(phoible, Glottocode)
+    glotto_invalid <- setdiff(glotto_phoible, glotto_valid)
+
+    testthat::expect_length(iso_invalid, 0)
+    testthat::expect_length(glotto_invalid, 0)
+    })
+
+
+testthat::test_that("metadata is unique within each inventory", {
+    ## extract metadata columns and count unique rows
+    phoible %>%
+        group_by(InventoryID) %>%
+        select(Glottocode, ISO6393, LanguageName, SpecificDialect, Source) %>%
+        n_distinct() ->
+        n_distinct_metadata_rows
+    ## count inventories
+    phoible %>%
+        select(InventoryID) %>%
+        n_distinct() ->
+        n_inventories
+    ## test
+    testthat::expect_equal(n_distinct_metadata_rows, n_inventories)
+    })
