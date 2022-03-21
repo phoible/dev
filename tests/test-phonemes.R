b66d3ffe1b16ed9b426e6a3f7d32e4cc1f96290a
Daniel McCloy <dan@mccloy.info>
2022-03-21 07:18:28 -0500

    Fix EA breathy (#347)
    * conventionalize EA (mostly breathy voicing)
    
    * regen data
    
    * add tests

--- tests/test-phonemes.R
+++ tests/test-phonemes.R
@@ -22,8 +22,48 @@ test_that("inventories don't have duplicate phonemes", {
         duplicates
 
     expect(nrow(duplicates) == 0,
-    	   paste(c("INVENTORIES WITH DUPLICATE PHONEMES:",
-    	   	       capture.output(print(duplicates))), sep="\n")
-    	   )
+           paste(c("INVENTORIES WITH DUPLICATE PHONEMES:",
+                   capture.output(print(duplicates))), sep="\n")
+           )
+    }
+)
+
+context("Features")
+
+test_that("feature vectors are consistently assigned", {
+    phoible %>%
+        group_by(Phoneme) %>%
+        select(Phoneme, tone:click) %>%
+        distinct() %>%
+        filter(n() > 1) ->
+        inconsistents
+
+    expect(nrow(inconsistents) == 0,
+           paste(c("PHONEMES WITH MULTIPLE DISTINCT FEATURE VECTORS:",
+                   capture.output(print(inconsistents))), sep="\n")
+           )
+    }
+)
+
+test_that("within inventories, phonemes have distinct feature vectors", {
+    phoible %>%
+        group_by(InventoryID) %>%
+        count(name="n_phonemes") ->
+        n_phonemes
+    phoible %>%
+        group_by(InventoryID) %>%
+        select(InventoryID, tone:click) %>%
+        distinct() %>%
+        count(name="n_distinct_feature_vectors") ->
+        n_vectors
+    n_phonemes %>%
+        full_join(n_vectors, by="InventoryID") %>%
+        filter(n_phonemes != n_distinct_feature_vectors) ->
+        mismatches
+
+    expect(nrow(mismatches) == 0,
+           paste(c("INVENTORIES WITH INDISTINCT FEATURE VECTORS:",
+                   capture.output(print(mismatches))), sep="\n")
+           )
     }
 )
