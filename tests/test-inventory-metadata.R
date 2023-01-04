76b24fd3e97bfbee0664387f344a81572e388608
Daniel McCloy <dan@mccloy.info>
2023-01-04 12:24:07 -0600

    fix some tests (#357)
    * WIP update inventory metadata tests
    
    * kxl -> kru
    https://iso639-3.sil.org/request/2019-048
    
    * krm -> bmf
    https://iso639-3.sil.org/request/2016-015
    
    * drh -> khk
    https://iso639-3.sil.org/request/2009-020
    
    * dit -> dif
    https://iso639-3.sil.org/request/2018-023
    
    * bjd -> drl
    https://iso639-3.sil.org/request/2011-145
    
    * add ISO code exceptions to test
    
    * rerun aggregation after rebase
    
    * split ISO and glottocode tests

--- tests/test-inventory-metadata.R
+++ tests/test-inventory-metadata.R
@@ -11,7 +11,7 @@
 ## http://www-01.sil.org/iso639-3/codes_retired.asp
 
 library(dplyr, warn.conflicts=FALSE)
-library(testthat)
+library(testthat, warn.conflicts=FALSE)
 
 context("Inventory metadata")
 
@@ -20,37 +20,150 @@ phoible_data_file  <- file.path("..", "data", "phoible.csv")
 phoible_col_types <- readr::cols(InventoryID="i", Marginal="l", .default="c")
 phoible <- readr::read_csv(phoible_data_file, col_types=phoible_col_types)
 
+default_char_cols <- readr::cols(.default="c")
 
-test_that("language codes are valid", {
+test_that("ISO codes are valid", {
     ## set global options (restored automatically on exit)
     withr::local_options(list(stringsAsFactors=FALSE))
 
-    default_char_cols <- readr::cols(.default="c")
-
     ## load official ISO table
     iso_url <- "http://www-01.sil.org/iso639-3/iso-639-3.tab"
-    iso_table <- readr::read_tsv(iso_url, col_types=default_char_cols)
+    iso_table <- readr::read_tsv(iso_url, col_types=default_char_cols,
+                                 trim_ws=FALSE)
     iso_valid <- pull(iso_table, Id)
 
+    ## list exceptions (invalid isocodes that we don't consider errors)
+    iso_exceptions <- c(
+        "daf",  # split: https://iso639-3.sil.org/request/2012-083
+                # checked; not clear which lx. the source describes
+
+        "duj",  # split: https://iso639-3.sil.org/request/2015-053
+                # source: https://glottolog.org/resource/reference/id/136243
+                # langoid: https://glottolog.org/resource/languoid/id/dhuw1249
+                # TODO: source title suggests this is `dwu`, but current
+                #     glottocode indicates that the source isn't actually that
+                #     specific. Check source to disambiguate if possible.
+
+        "ggr",  # split: https://iso639-3.sil.org/request/2012-138
+                # source: https://glottolog.org/resource/reference/id/73578
+                # langoid: https://glottolog.org/resource/languoid/id/aghu1254
+                # TODO: source title suggests this is `gtu` (one of the 3
+                #     possibilities from the split). Check source to
+                #     disambiguate if possible.
+
+        "kxu",  # split: http://www.iso639-3.sil.org/code/kxu
+                # source: https://glottolog.org/resource/reference/id/579765
+                # langoid: https://glottolog.org/resource/languoid/id/kuii1252
+                # TODO: see if source disambiguates: does it specifically
+                #     mention "Dawik region of Gajapati district"? -> `dwk`.
+                #     if it excludes that region, then `uki`.
+
+        "lno",  # split: https://iso639-3.sil.org/request/2021-027
+                # source: https://glottolog.org/resource/reference/id/13298
+                # source langoid:
+                #     https://glottolog.org/resource/languoid/id/lang1324
+                # our glottocode:
+                #     https://glottolog.org/resource/languoid/id/lang1322
+                # TODO: where did we get `lang1322`? If it's correct then we're
+                #     probably stuck with `lno` (unless the source
+                #     disambiguates); if glottolog is right then the ISO should
+                #     be `laj`
+
+        "mwd",  # split: http://www.iso639-3.sil.org/code/mwd
+                # source: https://glottolog.org/resource/reference/id/1921
+                # langoid: none associated with source nor current ISO code:
+                #     https://glottolog.org/glottolog?iso=mwd
+                # our current glottocode `mudb1240` is associated with one half
+                # of the split (`dmw`). The other half `xrq` (Karranga) has no
+                # associated langoid:
+                #     https://glottolog.org/glottolog?iso=xrq
+                # TODO: Double-check that the source really describes Mudburra
+                #     and not Karranga. If it describes both, change nothing
+                #     and keep this exception. If it describes only one, change
+                #     ISO (and possibly glottocode) accordingly.
+
+        "qgu",  # Extinct. https://glottolog.org/resource/languoid/id/wulg1239
+
+        "wit",  # split; https://iso639-3.sil.org/request/2012-144
+                # no glottocode for source:
+                #     https://glottolog.org/resource/reference/id/468848
+                # TODO check if source disambiguates
+
+        "wya",  # split: https://iso639-3.sil.org/request/2021-041
+                # source: https://glottolog.org/resource/reference/id/67646
+                # source languoid:
+                #     https://glottolog.org/resource/languoid/id/wyan1247
+                # our glottocode:
+                #     https://glottolog.org/resource/languoid/id/huro1249
+                # TODO: where did we get `huro1249`? If it's correct, then we
+                #     should update the ISO to `wdt`
+
+        "yiy"   # split: https://iso639-3.sil.org/request/2012-117
+                # source: https://glottolog.org/resource/reference/id/151393
+                # langoid: https://glottolog.org/resource/languoid/id/yiry1245
+                #
+                # NOTE: see note box on these two pages
+                #     https://glottolog.org/resource/languoid/id/jirj1239
+                #     https://glottolog.org/resource/languoid/id/dang1262
+                #     stating that according to *our source* the two lects that
+                #     this ISO was split into are "easily intelligible". It
+                #     might be worth looking at the source to see if it
+                #     actually reports data from both communities; if not then
+                #     we could justifiably update the ISO to one of the new
+                #     splits (`yyr` or `yrm`) and update the glottocode
+                #     accordingly. But it's likely that Glottolog already
+                #     checked this, so low priority.
+    )
+
+    ## pull out the relevant columns and compare to reference list
+    iso_phoible <- pull(phoible, ISO6393)
+    iso_invalid <- setdiff(iso_phoible, iso_valid)
+
+    ## test
+    iso_invalids_not_caught <- setdiff(iso_invalid, iso_exceptions)
+    iso_exceptions_not_needed <- setdiff(iso_exceptions, iso_invalid)
+    expect(length(iso_invalids_not_caught) == 0,
+           paste("INVALID ISO CODES NOT HANDLED:",
+                 paste(iso_invalids_not_caught, collapse=" "),
+                 sep="\n")
+           )
+    expect(length(iso_exceptions_not_needed) == 0,
+           paste("UNNECESSARY ISO CODE EXCEPTIONS:",
+                 paste(iso_exceptions_not_needed, collapse=" "),
+                 sep="\n")
+           )
+    }
+)
+
+
+test_that("glottocodes are valid", {
     ## load glottolog
     glotto_url <- "https://github.com/glottolog/glottolog-cldf/blob/master/cldf/languages.csv?raw=true"
     glotto_table <- readr::read_csv(glotto_url, col_types=default_char_cols)
     glotto_valid <- pull(glotto_table, ID)
 
-    ## pull out the relevant columns and compare to reference list
-    iso_phoible <- pull(phoible, ISO6393)
-    iso_invalid <- setdiff(iso_phoible, iso_valid)
     glotto_phoible <- pull(phoible, Glottocode)
     glotto_invalid <- setdiff(glotto_phoible, glotto_valid)
 
     ## test
-    expect(length(iso_invalid) == 0,
-           paste("INVALID ISO CODES:", paste(iso_invalid, collapse=" "),
+    expect(length(glotto_invalid) == 1 && all(is.na(glotto_invalid)),
+           paste("INVALID GLOTTOCODES:", paste(glotto_invalid, collapse=" "),
                  sep="\n")
            )
 
-    expect(length(glotto_invalid) == 0,
-           paste("INVALID GLOTTOCODES:", paste(glotto_invalid, collapse=" "),
+    ## Djindewal is our one missing glottocode
+    2729 -> djindewal_inventory_id
+    phoible %>%
+        filter(is.na(.$Glottocode)) %>%
+        select(InventoryID) %>%
+        distinct() %>%
+        pull() ->
+        na_glottocode_inventories
+    expect(length(na_glottocode_inventories) == 1 &&
+               na_glottocode_inventories == djindewal_inventory_id,
+           paste("UNEXPECTED NA GLOTTOCODES:",
+                 paste(setdiff(glotto_invalid, djindewal_inventory_id),
+                       collapse=" "),
                  sep="\n")
            )
     }
