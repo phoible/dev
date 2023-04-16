0fe3d3d9550d36460c05d72a997d38f2be326468
Daniel McCloy <dan@mccloy.info>
2023-04-16 13:52:16 -0500

    better testing and test output

--- tests/test-phonemes.R
+++ tests/test-phonemes.R
@@ -33,8 +33,7 @@ context("Features")
 test_that("feature vectors are consistently assigned", {
     phoible %>%
         group_by(Phoneme) %>%
-        select(Phoneme, tone:click) %>%
-        distinct() %>%
+        distinct(pick(tone:click)) %>%
         filter(n() > 1) ->
         inconsistents
 
@@ -47,26 +46,96 @@ test_that("feature vectors are consistently assigned", {
 
 test_that("within inventories, phonemes have distinct feature vectors", {
     phoible %>%
-        group_by(InventoryID) %>%
         filter(tone == "0") %>%
-        count(name="n_phonemes") ->
-        n_phonemes
-    phoible %>%
-        group_by(InventoryID) %>%
-        select(InventoryID, tone:click) %>%
-        filter(tone == "0") %>%
-        distinct() %>%
+        group_by(InventoryID) ->
+        grouped
+
+    # count distinct phonemes per inventory
+    grouped %>% count(name="n_phonemes") -> n_phonemes
+
+    # count distinct feature vectors per inventory
+    grouped %>%
+        distinct(pick(tone:click)) %>%
         count(name="n_distinct_feature_vectors") ->
         n_vectors
+
+    # tally mismatches between n_phonemes and n_feature_vectors
     n_phonemes %>%
         full_join(n_vectors, by="InventoryID") %>%
         filter(n_phonemes != n_distinct_feature_vectors) %>%
         mutate(difference=n_phonemes - n_distinct_feature_vectors) ->
         mismatches
 
+    if (nrow(mismatches)) {
+        # get DF with one row per collapsed phoneme contrast per inventory
+        "Phoneme" -> prefix
+        phoible %>%
+            filter(tone == "0", InventoryID %in% mismatches$InventoryID) %>%
+            group_by(pick(InventoryID, tone:click)) %>%
+            filter(n() > 1) %>%
+            mutate(id=row_number(),
+                   PhonemeList=list(stringr::str_sort(Phoneme))) %>%
+            tidyr::pivot_wider(
+                id_cols=c(InventoryID, tone:click, PhonemeList),
+                names_from=id, names_prefix=prefix, values_from=Phoneme) %>%
+            ungroup() %>%
+            select(InventoryID, starts_with(prefix), tone:click) ->
+            indistinct_phoneme_sets
+
+        # collapse across inventories to see just which contrasts need fixing
+        indistinct_phoneme_sets %>%
+            group_by(PhonemeList) %>%
+            mutate(InventoryIDs=list(InventoryID)) %>%
+            select(-InventoryID) %>%
+            distinct() %>%
+            ungroup() %>%
+            arrange(pick(starts_with(prefix))) %>%
+            relocate(InventoryIDs) ->
+            unique_indistinct_phoneme_sets
+
+        # ignore "known bugs"
+        list(
+            # voiced ejectives (check sources)
+            c("dʼ", "tʼ"),
+            c("dʼ", "tʰʼ"),
+            c("dzʼ", "tsʼ"),
+            c("d̠ʒʼ", "t̠ʃʼ"),
+            c("dʼkxʼ", "tʼkxʼ"),
+            c("d̪ʼkxʼ", "t̪ʼkxʼ"),
+            c("ɡʼ", "kʼ"),
+            # diacritic redundant / collapses contrast (check source)
+            c("kǀ", "kǀ̪", "kǃ̪"),
+            # doubled vowel (check source)
+            c("o", "oo"),
+            c("i", "ii"),
+            # glottalized vs creaky (check sources)
+            c("aˀ", "a̰"),
+            c("eˀ", "ḛ "),
+            c("iˀ", "ḭ"),
+            c("oˀ", "o̰"),
+            c("uˀ", "ṵ"),
+            # palatalized palatal (check sources)
+            c("c", "cʲ"),
+            c("ɟ", "ɟʲ"),
+            # voiceles aspiration on voiced glyph (fix)
+            c("d̠ʒʰ", "t̠ʃʰ")
+        ) -> known_bugs
+
+        # make print-friendly output
+        purrr::partial(stringr::str_c, collapse=" ") -> stringify
+        unique_indistinct_phoneme_sets %>%
+            filter(!PhonemeList %in% known_bugs) %>%
+            mutate(InventoryIDs=purrr::map_chr(InventoryIDs, stringify),
+                   Phonemes=purrr::map_chr(PhonemeList, stringify)) %>%
+            select(Phonemes, InventoryIDs) ->
+            unhandled_indistinct_phoneme_sets
+    }
+
     expect(nrow(mismatches) == 0,
-           paste(c("INVENTORIES WITH INDISTINCT FEATURE VECTORS:",
-                   capture.output(print(mismatches))), sep="\n")
+           paste(c("PHONEMES WITH INDISTINCT FEATURE VECTORS:",
+                   capture.output(print(unhandled_indistinct_phoneme_sets,
+                                        n=Inf))),
+                 sep="\n")
            )
     }
 )
